0b14292c99e37b1f8dbe0111b25d936c8384c9c0
Andrey <pepandrey12@gmail.com>
2025-07-10 08:08:58 -0600

    s

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    cssCodeSplit: true,
+    sourcemap: false,
+    outDir: 'dist',
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', '@mui/material'],
+          muiIcons: ['@mui/icons-material'],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['@mui/material', '@mui/icons-material'],
+    exclude: [],
+  },
+});
